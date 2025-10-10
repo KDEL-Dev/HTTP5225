0d8c5363f2367ea2ab22fd5adc2d44fbc956ae73
KDEL-Dev <lbrk0024@humber.ca>
2025-10-10 00:47:42 -0400

    comp lms work

---
+++ archive/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Schools</title>
+</head>
+<body>
+    <h1> Schools</h1>
+    <hr>
+    <div>
+        <?php
+            require('connect.php');
+
+            $query = 'SELECT * FROM schools';
+            $schools = mysqli_query($connect, $query);
+
+            // echo '<pre>' . print_r($schools) . '</pre>';
+
+            foreach($schools as $school)
+            {
+                echo $school['School Name'] . '<br>' .
+                '<form>
+                    <input type = "hidden" name="id" value="123">
+                    <input type="submit" value="EDIT">
+
+                </form>' .
+                '<br>';
+            }
+        ?>
+
+        <style>
+            .Box{
+                border:1px solid red;
+                padding: 20px;
+                display: inline-block;
+                box-sizing: border-box;
+            }
+        </style>
+</body>
+</html>
